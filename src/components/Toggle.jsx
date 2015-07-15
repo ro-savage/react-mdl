65a9efc44e32a7992e93a4fa8606ceb723837e6e
Ro Savage <rowan.savage@gmail.com>
2015-07-15 21:16:49 +1200

    In progress - Add radio button component

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,6 +1,7 @@
 "use strict";
 
 import React from "react"; // eslint-disable-line no-unused-vars
+import R from "ramda";
 import {
     component,
     mergeImplicitProps,
@@ -9,5 +10,37 @@ import {
 
 
 export default component(props => {
-    return <div>toggle</div>;
+
+    const {
+        type, // (flat), raised, icon, fab, mini-fab
+        color, // (none), primary, accent
+        disabled,
+        checked,
+        defaultChecked,
+        onChange
+        } = props;
+
+    // if checkbox is set to 'on', 'true' or true by the onChange event then make the checkbox checked
+    const isChecked = checked === "on" || checked === "true" || checked === true ? "checked" : "";
+
+    const className = buildClassName({
+        "mdl-button": true,
+        "mdl-js-button": true,
+        "mdl-js-ripple-effect": true,
+
+        // Default: uncolored
+        "mdl-button--colored": color === "primary",
+        "mdl-button--accent": color === "accent",
+
+        // Default: flat
+        "mdl-button--raised": type === "raised",
+        "mdl-button--icon": type === "icon",
+        "mdl-button--fab": type === "fab" || type === "mini-fab",
+        "mdl-button--mini-fab": type === "mini-fab"
+    });
+
+    return <label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-1">
+            <input type="checkbox" id="checkbox-1" className="mdl-checkbox__input" defaultChecked={defaultChecked} onChange={onChange} />
+            <span className="mdl-checkbox__label">Checkbox</span>
+        </label>;
 });
