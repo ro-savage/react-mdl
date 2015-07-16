cc8e6fe458c6aef671e6503275e8d19deaeccc42
Ro Savage <rowan.savage@gmail.com>
2015-07-16 14:50:14 +1200

    Bugfix for slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -19,7 +19,7 @@ export default component(props => {
     const upperSlider = 1 - lowerSlider;
 
     return <div className="mdl-slider__container" style={props.style}>
-        <input {...props} style={{ flex: "1 1 0%", height: "5em" } }} className="mdl-slider is-upgraded" type="range" />
+        <input {...props} style={{ flex: "1 1 0%", height: "5em" }} className="mdl-slider is-upgraded" type="range" />
         <div className="mdl-slider__background-flex">
             <div style={{ flex: lowerSlider + " 1 0%" }} className="mdl-slider__background-lower" />
             <div style={{ flex: upperSlider + " 1 0%" }} className="mdl-slider__background-upper" />
